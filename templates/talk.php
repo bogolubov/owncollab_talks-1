486cfb661a8723f1c57c1e97d1728ad55304ea34
olexiy <olexiy.bogolubov@gmail.com>
2016-03-21 11:02:05 +0200

    <?php

--- templates/talk.php
+++ templates/talk.php
@@ -1,5 +1,4 @@
 <?php
-//var_dump($_);
 style('files', 'files');
 style('owncollab_talks', 'trumbowyg/trumbowyg');
 script('owncollab_talks', 'trumbowyg/trumbowyg');
@@ -35,34 +34,6 @@ script('owncollab_talks', 'trumbowyg/trumbowyg');
 					break;
 				case 'save':
 					var_dump($_['saved']);
-					break;
-				case 'files':
-					?>
-				<table>
-					<tr>
-						<th>id</th>
-						<th>share_with</th>
-						<th>share_type</th>
-						<th>item_type</th>
-						<th>file_target</th>
-						<th>permissions</th>
-						<th>stime</th>
-					</tr>
-					<?
-					foreach($_['files'] as $f => $file) {
-						?>
-					<tr>
-						<td><?=$file['id'];?></td>
-						<td><?=$file['share_with'];?></td>
-						<td><?=$file['share_type'];?></td>
-						<td><?=$file['item_type'];?></td>
-						<td><?=$file['file_target'];?></td>
-						<td><?=$file['permissions'];?></td>
-						<td><?=date('d.m.Y H:i:s', $file['stime']);?></td>
-					</tr>
-					<? } ?>
-					</table>
-					<?
 					break;
 				default:
 					print_unescaped($this->inc('part.message-list'));
