faa4507b3eae3274de554c3990c8885962b85767
werdffelynir <werdffelynir@gmail.com>
2016-11-01 20:38:50 +0200

    fixed initit.py

--- initit.py
+++ initit.py
@@ -2,23 +2,45 @@
 
 
 import os
+import shutil
+
 
 dirs = ['temp', 'config']
+app_root = os.getcwd()
 
-for dr in dirs:
-    if os.path.isdir(dr):
-        os.chmod(dr, 0o777)
 
-create_files = ['mailparser.log', 'mailparser_error.log', 'config/config.php']
+if app_root.split('/')[-1] != 'owncollab_talks':
+    print("Error: owncollab_talks/config/config.php not exist in current directory")
+    exit()
 
+
+if not os.path.isdir(app_root + '/config'):
+    os.mkdir(app_root + '/config', 0o777)
+else:
+    os.chmod(app_root + '/config', 0o777)
+
+
+if os.path.isdir('temp'):
+    shutil.rmtree(app_root + '/temp')
+
+os.mkdir(app_root + '/temp', 0o777)
+
+
+create_files = [
+    'mailparser.log',
+    'mailparser_error.log',
+    'config/config.php'
+]
+
+
+# clean or create d files
 for fl in create_files:
-    if os.path.isfile(fl):
-        file = open(fl, 'w+')
-        file.write('')
-        file.close()
+    file = open(app_root + '/' + fl, 'w+')
+    file.write('')
+    file.close()
 
 
-config_file = open('config/config.php', 'w+')
+config_file = open(app_root + '/config/config.php', 'w+')
 config_data = '''<?php
 
 return [
@@ -35,3 +57,5 @@ return [
 
 config_file.write(config_data)
 config_file.close()
+
+print("Script executed")
